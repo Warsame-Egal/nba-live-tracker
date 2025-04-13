8a965a7eaa1f5c0cd73cb54fd10289408746934e
Warsame Egal <warsameegal0@gmail.com>
2025-04-13 18:39:18 -0400

    add render url

--- nba-tracker/vite.config.ts
+++ nba-tracker/vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: ['nba-frontend-fcd3.onrender.com'], // Allow external access from this Render domain.
+  },
 });
